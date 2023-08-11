cf5b0da3afccc3cd5017c35a31f2610ff5935f93
David Young <davidsamuelyoung@protonmail.com>
2023-08-11 15:33:51 +0200

    Added WorkPackage1

---
+++ WorkPackage1/C/src/CHeterodyning.c
@@ -0,0 +1,42 @@
+#include "CHeterodyning.h"
+
+extern float data [SAMPLE_COUNT];
+extern float carrier[SAMPLE_COUNT];
+
+float result [SAMPLE_COUNT];
+
+int main(int argc, char**argv){
+    printf("Running Unthreaded Test\n");
+    printf("Precision sizeof %ld\n", sizeof(float));
+    
+
+    printf("Total amount of samples: %ld\n", sizeof(data) / sizeof(data[0]));
+    
+
+    tic(); // start the timer
+    for (int i = 0;i<SAMPLE_COUNT;i++ ){
+        result[i] = data[i] * carrier[i];
+    }
+    double t = toc();
+    printf("Time: %lf ms\n",t/1e-3);
+    printf("End Unthreaded Test\n");
+
+    // Write result array to txt file
+    FILE *f = fopen("result.txt", "w");
+    if (f == NULL)
+    {
+        printf("Error opening file!\n");
+        exit(1);
+    }
+
+    // print array to file
+    for (int i = 0; i < SAMPLE_COUNT; i++)
+    {
+        fprintf(f, "%f\n", result[i]);
+    }
+
+    // close file
+    fclose(f);
+
+    return 0;
+}
