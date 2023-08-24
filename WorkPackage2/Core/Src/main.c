7dc26a25e526e4cbd0ff6160c087ad7e9d25ae32
David Young <davidsamuelyoung@protonmail.com>
2023-08-24 13:02:17 +0200

    Finished all tasks

--- WorkPackage2/Core/Src/main.c
+++ WorkPackage2/Core/Src/main.c
@@ -53,39 +53,8 @@ TIM_HandleTypeDef htim16;
 
 /* USER CODE BEGIN PV */
 // TODO: Define any input variables
-// An array of 8-bit integers that holds the following binary values: 10101010, 01010101, 11001100, 00110011, 11110000, 00001111
-//static uint8_t patterns[] = {0xAA, 0x55, 0xCC, 0x33, 0xF0, 0x0F};  // TASK 1
-// Create anotehr array of 8-bit integers that holds the binary values counting from 0 to 255 (i.e. 0b00000000, 0b00000001, 0b00000010, ..., 0b11111111)
-static uint8_t patterns[] = {
-  0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 
-  0x0A, 0x0B, 0x0C, 0x0D, 0x0E, 0x0F, 0x10, 0x11, 0x12, 0x13,
-  0x14, 0x15, 0x16, 0x17, 0x18, 0x19, 0x1A, 0x1B, 0x1C, 0x1D,
-  0x1E, 0x1F, 0x20, 0x21, 0x22, 0x23, 0x24, 0x25, 0x26, 0x27,
-  0x28, 0x29, 0x2A, 0x2B, 0x2C, 0x2D, 0x2E, 0x2F, 0x30, 0x31,
-  0x32, 0x33, 0x34, 0x35, 0x36, 0x37, 0x38, 0x39, 0x3A, 0x3B,
-  0x3C, 0x3D, 0x3E, 0x3F, 0x40, 0x41, 0x42, 0x43, 0x44, 0x45,
-  0x46, 0x47, 0x48, 0x49, 0x4A, 0x4B, 0x4C, 0x4D, 0x4E, 0x4F,
-  0x50, 0x51, 0x52, 0x53, 0x54, 0x55, 0x56, 0x57, 0x58, 0x59,
-  0x5A, 0x5B, 0x5C, 0x5D, 0x5E, 0x5F, 0x60, 0x61, 0x62, 0x63,
-  0x64, 0x65, 0x66, 0x67, 0x68, 0x69, 0x6A, 0x6B, 0x6C, 0x6D,
-  0x6E, 0x6F, 0x70, 0x71, 0x72, 0x73, 0x74, 0x75, 0x76, 0x77,
-  0x78, 0x79, 0x7A, 0x7B, 0x7C, 0x7D, 0x7E, 0x7F, 0x80, 0x81,
-  0x82, 0x83, 0x84, 0x85, 0x86, 0x87, 0x88, 0x89, 0x8A, 0x8B,
-  0x8C, 0x8D, 0x8E, 0x8F, 0x90, 0x91, 0x92, 0x93, 0x94, 0x95,
-  0x96, 0x97, 0x98, 0x99, 0x9A, 0x9B, 0x9C, 0x9D, 0x9E, 0x9F,
-  0xA0, 0xA1, 0xA2, 0xA3, 0xA4, 0xA5, 0xA6, 0xA7, 0xA8, 0xA9,
-  0xAA, 0xAB, 0xAC, 0xAD, 0xAE, 0xAF, 0xB0, 0xB1, 0xB2, 0xB3,
-  0xB4, 0xB5, 0xB6, 0xB7, 0xB8, 0xB9, 0xBA, 0xBB, 0xBC, 0xBD,
-  0xBE, 0xBF, 0xC0, 0xC1, 0xC2, 0xC3, 0xC4, 0xC5, 0xC6, 0xC7,
-  0xC8, 0xC9, 0xCA, 0xCB, 0xCC, 0xCD, 0xCE, 0xCF, 0xD0, 0xD1,
-  0xD2, 0xD3, 0xD4, 0xD5, 0xD6, 0xD7, 0xD8, 0xD9, 0xDA, 0xDB,
-  0xDC, 0xDD, 0xDE, 0xDF, 0xE0, 0xE1, 0xE2, 0xE3, 0xE4, 0xE5,
-  0xE6, 0xE7, 0xE8, 0xE9, 0xEA, 0xEB, 0xEC, 0xED, 0xEE, 0xEF,
-  0xF0, 0xF1, 0xF2, 0xF3, 0xF4, 0xF5, 0xF6, 0xF7, 0xF8, 0xF9,
-  0xFA, 0xFB, 0xFC, 0xFD, 0xFE, 0xFF
-};
-// Variable to keep track of what delay to use
-//int16_t currentDelay = 1000;  // TASK 6
+static uint8_t patterns[] = {0xAA, 0x55, 0xCC, 0x33, 0xF0, 0x0F};  // TASK 1
+uint32_t currentDelay = 1000;
 
 /* USER CODE END PV */
 
@@ -137,15 +106,10 @@ int main(void)
   /* USER CODE BEGIN 2 */
 
   // TODO: Start timer TIM16
-  // A timer (TIM16) has already been configured and initialised for you, including an interrupt called "TIM16_IRQHandler. 
-  // As part of your main function, start the TIM16 process in interrupt (IT) mode.
-  // HINT: The HAL library will be useful here, particularly one of the " HAL_TIM" functions.
   HAL_TIM_Base_Start_IT(&htim16);  // TASK 2
+  htim16.Instance->ARR = 1000-1;  // Set the default timer delay to 1 second
 
   // TODO: Write all "patterns" to EEPROM using SPI
-  // SPI has already been initialised for you as part of the main function.
-  // Write code that takes your array of binary values and writes them all to EEPROM using SPI;
-  // use the provided write_to_address function as part of your work, taking note of the variable types involved.
   for (int i = 0; i < sizeof(patterns); i++) {
     write_to_address(i, patterns[i]);  // TASK 3
   }
@@ -161,20 +125,21 @@ int main(void)
     /* USER CODE BEGIN 3 */
 
 	  // TODO: Check button PA0; if pressed, change timer delay
-    // TASK 6
-    // TIM16 has been configured to trigger an interrupt every 1 second. 
-    // As part of your main function's while loop, write code to alternate between 
-    // this 1-second delay and a half-second delay whenever Pushbutton 0 (pin PA0) is pressed. 
-    // Use TIM16's ARR value in your implementation.
-
     // Check if the button is pressed
-    if (LL_GPIO_IsInputPinSet(Button0_GPIO_Port, Button0_Pin)) {
-      // Set the timer to 0.5 seconds
-      htim16.Instance->ARR = 100-1;
-    } else {
-      // Set the timer to 1 second
-      htim16.Instance->ARR = 500-1;
+    if (LL_GPIO_IsInputPinSet(Button0_GPIO_Port, Button0_Pin) == 0) {
+      if (currentDelay == 500) {
+        // Set the timer to 1 second
+        currentDelay = 1000;
+      } else {
+        // Set the timer to 0.5 seconds
+        currentDelay = 500;
+      }
     }
+
+    htim16.Instance->ARR = currentDelay - 1;  // TASK 6
+
+    // Delay to prevent button bouncing - UNUSED
+    //delay(10000);
   }
   /* USER CODE END 3 */
 }
@@ -483,15 +448,8 @@ void TIM16_IRQHandler(void)
 
 	// TODO: Change to next LED pattern; output 0x01 if the read SPI data is incorrect
   // TASK 4
-  // The "TIM16_IRQHandler" function is invoked whenever the timer interrupt occurs (approximately every 1 second).
-  // Use the provided "read_from_address" function to read the next binary value of your array from EEPROM.
-  // HINT: You will need some way to keep track of the binary array index 
-  // (i.e. the "address" input required by this function), so include logic to cycle 
-  // to the next binary value each time the interrupt occurs. Write the returned binary value to the LEDs.
 
   // TASK 5
-  // Include failsafe code in your interrupt that checks whether the 8-bit value read from 
-  // EEPROM corresponds to the correct value in your array; if not, output 0b00000001 to your LEDs to indicate an SPI failure.
   static uint16_t currentIndex = 0; // Keep track of the current index
   uint8_t data = read_from_address(currentIndex); // Read the data from the current index
 
